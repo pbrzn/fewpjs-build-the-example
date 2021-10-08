8f2bd2b87c91152f05f655fe950baa6fb1c6fc91
Patrick Brennan <pbrennanmusic@gmail.com>
2021-10-08 00:44:15 -0400

    commit tree 7f17569cdb41902885f070d3eff0cf6e032f0d3b

--- main.js
+++ main.js
@@ -4,6 +4,43 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+function changeHeart(icon) {
+  if (icon === EMPTY_HEART){
+    icon = FULL_HEART;
+  } else { icon = EMPTY_HEART }
+}
+
+const allHearts = document.querySelectorAll('span.like-glyph')
+
+allHearts.forEach(heart => {
+  heart.addEventListener("click", () => {
+    // const config = {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-type": "application/json"
+    //     Accept: "application/json"
+    //   },
+    //   body: {
+    //     "like-glyph": FULL_HEART
+    //   }
+    // };
+
+    fetch(mimicServerCall(url, config))
+    .then(resp => resp.json())
+    .then(json => {
+      changeHeart(heart);
+      alert('You changed the heart!')
+    })
+    .catch(error => {
+      const modal = document.querySelector('div#modal');
+      modal.classList.remove('hidden');
+      const p = document.querySelector('p#modal-message')
+      p.innerHTML = error;
+
+      setTimeout((modal.ClassName = 'hidden'), 3000)
+    })
+  })
+})
 
 
 
